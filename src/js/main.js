d93a72b6824e8304b515b954d7c8cf6269f07d15
philippbruhin <info@philippbruhin.ch>
2018-05-30 22:11:50 +0200

    basic styles

---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+$(function() {
+
+
+    // Define the shuffle function
+    function shuffle(o){
+        for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+    };
+        
+    // Define backgrounds array
+    var bgimages = [
+        { src: 'img/background-01.jpg' },
+        { src: 'img/background-02.jpg' },
+        { src: 'img/background-03.jpg' },
+        { src: 'img/background-04.jpg' },
+        { src: 'img/background-05.jpg' },
+        { src: 'img/background-06.jpg' },
+        { src: 'img/background-07.jpg' },
+        { src: 'img/background-08.jpg' },
+        { src: 'img/background-09.jpg' },
+        { src: 'img/background-10.jpg' },
+    ]
+    
+    // Suffle the array
+    randombgs = shuffle(bgimages);
+
+
+
+    $('body').vegas({
+        timer: false,
+        transition: 'fade',
+        delay: 3000,
+        cover: true,
+        slides: randombgs
+    });
+});
